0d8dcced6fd8fc43efd6bb55b14fea51560d3d93
venom1270 <zigsim1@gmail.com>
2016-12-11 16:47:11 +0100

    Testing Raycasting and object moving

---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                //Debug.DrawLine(ray.origin, hit.point);
+                GameObject.Find("Ethan").transform.position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+                //(hit.point);
+            }
+            else
+                print("Krscen maticek");
+        }
+            
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
