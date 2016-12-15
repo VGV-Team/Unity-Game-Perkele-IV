1343adafe2a091f3ffd2595b10a34ab343e8e27c
nsk4 <nejcsk@hotmail.com>
2016-12-15 11:26:43 +0100

    added simple UI logics

---
+++ Assets/Prefabs/UI/UIScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+	public Text MoneyText;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	
+	public void SpendMoneyOnClick()
+	{
+		print("qwe");
+		MoneyText.text = ""+ (int.Parse(MoneyText.text) - 10);
+	}
+
+	public void GetMoneyOnClick()
+	{
+		MoneyText.text = "" + (int.Parse(MoneyText.text) + 1);
+	}
+
+}
