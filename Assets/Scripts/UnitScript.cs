eba1399753e7f86edc1c3d3d3cd25f72d6d1d7fc
Nejc Smrkolj Kozelj <nejcsk@hotmail.com>
2016-12-27 00:05:35 +0100

    added few script files

---
+++ Assets/Scripts/UnitScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitScript : EntityScript
+{
+    public int HP;
+    public int HPRegen;
+    public int Armor;
+    public int EnergyShield;
+    public int Strenth;
+    public int Fury;
+    public int Mana;
+    public int ManaRegen;
+    
+    public int Level;
+    public int Xp;
+
+    // public Waypoint DestinationWaypoint
+    
+
+    // TODO: Add abilities
+    public List<AbilityScript> Abilities;
+
+    public void Attack()
+    {
+        
+    }
+
+    public void TakeDamage()
+    {
+        
+    }
+}
