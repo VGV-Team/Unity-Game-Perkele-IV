15392f559bb1ecb72d8efaaed9dfbe868ba1bcfc
Nejc Smrkolj Kozelj <nejcsk@hotmail.com>
2016-12-27 02:32:36 +0100

    Added basic UI for player basic stats

--- Assets/Scripts/UI/UIScript.cs
+++ Assets/Scripts/UI/UIScript.cs
@@ -6,23 +6,112 @@ using UnityEngine.UI;
 
 public class UIScript : MonoBehaviour
 {
-    private GameObject ActivePlayer;
-
-	public Text MoneyText;
+    private PlayerScript ActivePlayer;
 
 
-	// Use this for initialization
-	void Start () {
-		ActivePlayer = GameObject.Find("Player");
-	}
+    private GameObject UIHPBar;
+    private GameObject UIHPValueLabel;
+    private GameObject UIShieldBar;
+    private GameObject UIShieldValueLabel;
+    private GameObject UIManaBar;
+    private GameObject UIManaValueLabel;
+    private GameObject UIFuryBar;
+    private GameObject UIFuryValueLabel;
+
+    private GameObject UIXPBar;
+    private GameObject UIXPValueLabel;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		ActivePlayer = (PlayerScript)GameObject.Find("Player").GetComponent("PlayerScript");
+        UIHPBar = GameObject.Find("UIHPBar");
+        UIHPValueLabel = GameObject.Find("UIHPValueLabel");
+        UIShieldBar = GameObject.Find("UIShieldBar");
+        UIShieldValueLabel = GameObject.Find("UIShieldValueLabel");
+        UIManaBar = GameObject.Find("UIManaBar");
+        UIManaValueLabel = GameObject.Find("UIManaValueLabel");
+        UIFuryBar = GameObject.Find("UIFuryBar");
+        UIFuryValueLabel = GameObject.Find("UIFuryValueLabel");
+        UIXPBar = GameObject.Find("UIXPBar");
+        UIXPValueLabel = GameObject.Find("UIXPValueLabel");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        UpdateUIBars();
+    }
 
-	
-	public void SpendMoneyOnClick()
+
+    private void UpdateUIBars()
+    {
+        if (ActivePlayer.MaxHP > 0)
+        {
+            float HPScale = ActivePlayer.HP / (float)ActivePlayer.MaxHP;
+            if (HPScale > 1) HPScale = 1;
+            UIHPBar.transform.localScale = new Vector3(1, HPScale, 1);
+        }
+        else
+        {
+            UIHPBar.transform.localScale = new Vector3(1, 0, 1);
+        }
+        UIHPValueLabel.GetComponent<Text>().text = ActivePlayer.HP + " / " + ActivePlayer.MaxHP;
+
+        if (ActivePlayer.MaxShield > 0)
+        {
+            float shieldScale = ActivePlayer.Shield / (float)ActivePlayer.MaxShield;
+            if (shieldScale > 1) shieldScale = 1;
+            UIShieldBar.transform.localScale = new Vector3(1, shieldScale, 1);
+        }
+        else
+        {
+            UIShieldBar.transform.localScale = new Vector3(1, 0, 1);
+        }
+        UIShieldValueLabel.GetComponent<Text>().text = ActivePlayer.Shield + " / " + ActivePlayer.MaxShield;
+
+        if (ActivePlayer.MaxMana > 0)
+        {
+            float manaScale = ActivePlayer.Mana / (float)ActivePlayer.MaxMana;
+            if (manaScale > 1) manaScale = 1;
+            UIManaBar.transform.localScale = new Vector3(1, manaScale, 1);
+        }
+        else
+        {
+            UIManaBar.transform.localScale = new Vector3(1, 0, 1);
+        }
+        UIManaValueLabel.GetComponent<Text>().text = ActivePlayer.Mana + " / " + ActivePlayer.MaxMana;
+
+        if (ActivePlayer.MaxFury > 0)
+        {
+            float furyScale = ActivePlayer.Fury / (float)ActivePlayer.MaxFury;
+            if (furyScale > 1) furyScale = 1;
+            UIFuryBar.transform.localScale = new Vector3(1, furyScale, 1);
+        }
+        else
+        {
+            UIFuryBar.transform.localScale = new Vector3(1, 0, 1);
+        }
+        UIFuryValueLabel.GetComponent<Text>().text = ActivePlayer.Fury + " / " + ActivePlayer.MaxFury;
+
+
+        if (ActivePlayer.MaxXp > 0)
+        {
+            float XPScale = ActivePlayer.Xp / (float)ActivePlayer.MaxXp;
+            if (XPScale > 1) XPScale = 1;
+            UIXPBar.transform.localScale = new Vector3(XPScale, 1, 1);
+        }
+        else
+        {
+            UIXPBar.transform.localScale = new Vector3(0, 1, 1);
+        }
+        UIXPValueLabel.GetComponent<Text>().text = ActivePlayer.Xp + " / " + ActivePlayer.MaxXp;
+    }
+
+
+    public Text MoneyText;
+    public void SpendMoneyOnClick()
 	{
 		print("qwe");
 		MoneyText.text = ""+ (int.Parse(MoneyText.text) - 10);
--- Assets/Scripts/UnitScript.cs
+++ Assets/Scripts/UnitScript.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class UnitScript : EntityScript
 {
+    public int MaxHP;
     public int HP;
     public int HPRegen;
     public int Armor;
-    public int EnergyShield;
+    public int MaxShield;
+    public int Shield;
     public int Strenth;
+    public int MaxFury;
     public int Fury;
+    public int FuryDecay;
+    public int MaxMana;
     public int Mana;
     public int ManaRegen;
     
     public int Level;
     public int Xp;
+    public int MaxXp;
 
     // public Waypoint DestinationWaypoint
     
