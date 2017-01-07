a1cf6b7715e6a66e3f3ecaa97ea17a777fadb55c
Nejc Smrkolj Kozelj <nejcsk@hotmail.com>
2017-01-07 02:57:52 +0100

    implemented menu system, closes #6

---
+++ Assets/Scripts/UI/UIMainMenuScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIMainMenuScript : MonoBehaviour
+{
+
+	public GameObject VolumeSlider;
+	public GameObject LoadingSlider;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		AudioListener.volume = VolumeSlider.GetComponent<Slider>().value;
+	}
+
+	public void ExitApplication()
+	{
+		#if UNITY_EDITOR
+				UnityEditor.EditorApplication.isPlaying = false;
+		#else
+				Application.Quit();
+		#endif
+	}
+
+	private AsyncOperation async;
+
+	IEnumerator LoadLevelWithBar ()
+	{
+		async = SceneManager.LoadSceneAsync("MainScene");
+		while (!async.isDone)
+		{
+			LoadingSlider.GetComponent<Slider>().value = async.progress;
+			yield return null;
+		}
+	}
+	
+
+	public void StartGame()
+	{
+		LoadingSlider.GetComponent<Slider>().value = 0;
+		StartCoroutine(LoadLevelWithBar());
+	}
+}
